bd9d8f5e9edc525762e1ac3cc5bcb36dcedfaa66
MIRZA MOIZ BAIG <baigmirzamoiz1@gmail.com>
2026-09-11 20:51:23 -0600

    lesson 03 started/ lesson -02 ended

--- inclass/03/demo.js
+++ inclass/03/demo.js
@@ -1,3 +1,28 @@
 console.log('Lesson 03 demo.js has loaded');
 console.log('=============================');
 console.log();
+// Mirza Moiz Baig - Sept 11,2026
+console.log('Primitive Values');
+console.log('----------------');
+
+let workshopTitle = 'Intro to Digital photography';
+let seatsAvailable = 18;
+let registrationOpen = true; //This is a boolean(bool) value
+// true or false are the only boolean values
+const registrationClosed = false;
+
+console.log(typeof workshopTitle);
+console.log(typeof seatsAvailable);
+console.log(typeof registrationOpen);
+
+console.log('Object Literals');
+let workshop = {
+    title: 'Intro to Digital Photography',
+    room: 'Media lab',
+    capacity: 16,
+    registered: 11,
+    isOnline: false
+ }
+ // object literal consists of key-value pairs
+ // The value on the left of semicolons is the key and the value on the right is value.
+
